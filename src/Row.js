f4390b08b438e1a7eb2f22e5303531b36cc2fc35
JhohannesK <jdomeh77@gmail.com>
2022-04-03 17:37:11 +0000

    gitignore

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios'
 import './Row.css'
-import YouTube from 'react-youtube'
-import movieTrailer from 'movie-trailer'
 
 const base_url = 'https://image.tmdb.org/t/p/original/'
 
 function Row({ title, fetchUrl, isLargeRow }) {
       const [movies, setMovies] = useState([]);
-      const [trailerUrl, setTrailerUrl] = useState("")
+
 
       useEffect(() => {
             // if [] , run once when the row loads, and don't run again
